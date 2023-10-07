858d89f2f9ee57661fb05d082b6cbaa5f300b3d9
selbin <selbinzr@gmail.com>
2023-10-07 12:13:09 +0530

    implemented db retry logic

--- src/database/db.js
+++ src/database/db.js
@@ -1,9 +1,20 @@
 import mongoose from 'mongoose';
 
-const dbConnect = async () => {
-    return await mongoose.connect(process.env.MONGODB_URI, {
-        minPoolSize: 5
-    });
+const dbConnect = async (retryInterval = 60000, maxRetries = 5) => {
+    // 1. connect to database
+    // 2. If connection fails, we try to retry for a specified limit within in a given interval of time
+    try {
+        return await mongoose.connect(process.env.MONGODB_URI, {
+            minPoolSize: 5
+        });
+    } catch (error) {
+        console.error('MongoDB connection error:', error.message);
+        if (maxRetries > 0) {
+            setTimeout(() => dbConnect(retryInterval, maxRetries - 1), retryInterval);
+        } else {
+            console.error('Maximum number of retries reached');
+        }
+    }
 };
 
 const db = mongoose.connection;
@@ -12,10 +23,10 @@ db.on('error', (error) => {
     console.error(error);
 });
 
-db.on('reconnected',()=>{
-    //will automatically reconnect on disconnection
-    console.log("reconnected db ....");
-})
+db.on('reconnected', () => {
+    //will automatically reconnect on disconnection after initial connection.
+    console.log('reconnected db ....');
+});
 
 db.once('open', () => {
     console.log('Connected to MongoDB database');
