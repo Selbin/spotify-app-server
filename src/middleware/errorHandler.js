4cbc4334d066e906b29664ec02e78da63c90ee47
selbin <selbinzr@gmail.com>
2023-10-07 21:23:12 +0530

    refactored code

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -2,12 +2,12 @@ import logger from "../logger/logger.js";
 
 export const errorHandler = (err, req, res, next) => {
     // Sent response based on error
+    logger.error(err.message)
     if (err.message === 'Invalid email or password') {
         return res.status(401).json({ message: 'Invalid email or password' });
     } else if (err.message === 'Email is already in use') {
         return res.status(422).json({ message: err.message });
     } else {
-        logger.error(err)
         return res.status(500).json({ message: 'Something went wrong' });
     }
 };
