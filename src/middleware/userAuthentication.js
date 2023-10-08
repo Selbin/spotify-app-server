753709b2b3706a630adcf9e1e436cb360ab0168c
selbin <selbinzr@gmail.com>
2023-10-08 09:28:49 +0530

    add jwt authentication for spotify routes

--- src/middleware/userAuthentication.js
+++ src/middleware/userAuthentication.js
@@ -1,19 +1,22 @@
 import jwt from 'jsonwebtoken';
 
-const jwtSecretKey = process.env.JWT_SECRET;
+import User from '../model/user.js';
 
-const authenticateUser = (req, res, next) => {
+const authenticateUser = async (req, res, next) => {
     try {
-        let token = req.get('authorization').split(' ')[1];
-        if (!token) {
-            res.status(404).json({ message: 'Invalid access token' });
+        const bearerToken = req.get('authorization');
+        if (!bearerToken) {
+            return res.status(404).json({ message: 'Invalid access token' });
         }
-        const decodedValue = jwt.verify(token, jwtSecretKey);
+        const token = bearerToken.split(' ')[1];
+        const value = jwt.decode(token);
+        const user = await User.findOne({ email: value.email });
+        const decodedValue = jwt.verify(token, user.accessTokenKey);
         req.user = decodedValue;
         next();
     } catch (error) {
-        return res.status(401).json({ message: error.message });
+        return res.status(401).json({ message: "Unauthorized" });
     }
 };
 
-export default authenticateUser
+export default authenticateUser;
--- src/routes/spotify.js
+++ src/routes/spotify.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
 import spotifyController from '../controllers/spotify.js';
+import authenticateUser from '../middleware/userAuthentication.js';
 
 const router = express.Router();
 
-router.get('/user/:id', spotifyController.getProfile);
-router.get('/playlists/:id', spotifyController.getPlaylist);
+router.get('/user/:id', authenticateUser, spotifyController.getProfile);
+router.get('/playlists/:id', authenticateUser, spotifyController.getPlaylist);
 
 export default router;
